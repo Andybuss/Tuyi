66ee0c5cbdebcaed6d72caddcb28828b91612617
donlan <760625325@qq.com>
2016-10-26 19:59:22 +0800

    add String field "description"

--- app/src/main/java/dong/lan/tuyi/bean/Album.java
+++ app/src/main/java/dong/lan/tuyi/bean/Album.java
@@ -5,12 +5,24 @@ import java.util.List;
 import cn.bmob.v3.BmobObject;
 
 /**
- * Created by 梁桂栋 on 2015/11/2.
+ * Created by 梁桂栋 on 2015/11/2 ： 下午12:58.
+ * Email:       760625325@qq.com
+ * GitHub:      github.com/donlan
+ * description: Tuyi
  */
 public class Album extends BmobObject {
-    TUser user;
-    List<UserTuyi> tuyis;
-    String musicType;
+    private String description;
+    private TUser user;
+    private List<UserTuyi> tuyis;
+    private  String musicType;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
     public String getMusicType() {
         return musicType;
